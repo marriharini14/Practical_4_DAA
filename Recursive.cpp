06ed1da2c5e02f4d13ed1c2505aec0a223280bb2
marriharini14 <marriharini9@gmail.com>
2026-08-19 17:06:02 +0530

    Change factorial function return type and update input prompts

--- Recursive.cpp
+++ Recursive.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-long long factorial(int n) {
+unsigned long long factorial(int n) {
     if (n == 0 || n == 1) {
         return 1;
     }
@@ -11,15 +11,16 @@ long long factorial(int n) {
 int main() {
     int num;
 
-    cout << "Enter a positive integer: ";
+    cout << "Enter a non-negative integer: ";
     cin >> num;
 
     if (num < 0) {
         cout << "Error: Factorial is not defined for negative numbers." << endl;
+    } else if (num > 20) { 
+        cout << "Error: Number too large. Please enter a value between 0 and 20." << endl;
     } else {
         cout << "Factorial of " << num << " = " << factorial(num) << endl;
     }
 
     return 0;
 }
-
